454effaa71a5de345ad9292b703a5432124e3150
Jason Litchney <jason@allstartalent.us>
2026-06-07 10:32:50 -0700

    Add debug mode to NextAuth to diagnose OAuthCallback error

--- dashboard/src/lib/auth-options.ts
+++ dashboard/src/lib/auth-options.ts
@@ -3,6 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
+  debug: true,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
